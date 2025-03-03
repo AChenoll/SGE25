8e08a8025f42dd83019abbe37669a6de23760675
AChenoll <achg7396@gmail.com>
2025-03-03 19:02:57 +0100

    T5.5 a medias

---
+++ src/app/services/unidades-centro.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { ApiResponse } from '../shared/interfaces/api-response';
+import { HttpClient } from '@angular/common/http';
+import { CommonService } from '../shared/common.service';
+import { URL_API } from 'src/environments/environment';
+import { UnidadesCentro } from '../shared/interfaces/unidades-centro';
+
+const ENDPOINT = 'unidad_centro';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnidadesCentroService {
+
+  unidadCentro: UnidadesCentro[];
+
+  constructor(private http: HttpClient, private commonService: CommonService) { }
+
+  get() {
+    return this.http.get<ApiResponse>(`${URL_API}/${ENDPOINT}.php`, { headers: this.commonService.headers });
+  }
+
+  getAllUnidadesCentro() {
+    return this.http.get<ApiResponse>(`${URL_API}/${ENDPOINT}.php`, { headers: this.commonService.headers });
+  }
+
+  addUnidadCentro(unidad_centro: UnidadesCentro) {
+    const body = JSON.stringify(unidad_centro);
+    return this.http.post<ApiResponse>(`${URL_API}/${ENDPOINT}.php`, body, { headers: this.commonService.headers });
+  }
+
+  editUnidad(unidad_centro: UnidadesCentro) {
+    const body = JSON.stringify(unidad_centro);
+    return this.http.put<ApiResponse>(`${URL_API}/${ENDPOINT}.php`, body, { headers: this.commonService.headers });
+  }
+
+  deleteUnidad(id: number|string) {
+    return this.http.delete<ApiResponse>(`${URL_API}/${ENDPOINT}.php?id=${id}`, {headers: this.commonService.headers });
+  }
+}
